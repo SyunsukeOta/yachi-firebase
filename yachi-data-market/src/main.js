a72d2de4023a895a71dbea0207ce452fa76dc08c
kibaota <ohta.syunsuke@liveout.co.jp>
2020-09-26 17:14:06 +0900

    firebaseにあるデータをvuefireを使ってhtml側で表示。

--- yachi-data-market/src/main.js
+++ yachi-data-market/src/main.js
@@ -2,6 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { firestorePlugin } from 'vuefire'
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+Vue.use(firestorePlugin)
+firebase.initializeApp({
+  apiKey: "AIzaSyBA65WosCaoIhK6gjUmRJTY7Pif2r0k2kc",
+  authDomain: "yachi-sample.firebaseapp.com",
+  databaseURL: "https://yachi-sample.firebaseio.com",
+  projectId: "yachi-sample",
+  storageBucket: "yachi-sample.appspot.com",
+  messagingSenderId: "179725569178",
+  appId: "1:179725569178:web:622de2cd87082093a0a323",
+  measurementId: "G-Z4FE2N7D2Z"
+})
+export const db = firebase.firestore()
+export const auth = firebase.auth()
+
 Vue.config.productionTip = false
 
 new Vue({
--- yachi-data-market/src/views/Home.vue
+++ yachi-data-market/src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    
+    <ul style="list-style: none;
+">
+      <li v-for="user in users" :key="user.id">
+        <p>名前:{{ user.name }}、スコア:{{ user.score }}、ヨットができたか?:{{ user.yachi }}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+// import HelloWorld from '@/components/HelloWorld.vue'
+import { db } from '@/main'
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      users: []
+    }
+  },
+  firestore() {
+    return {
+      users: db.collection('users')
+    }
+  },
+  
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
